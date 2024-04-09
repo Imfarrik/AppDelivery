6a6c2c30b177d389528e01f26ff50052385359c5
Farrukh Yu <imfarrik@mail.ru>
2024-04-09 22:50:46 +0500

    fix workflow

--- ui/src/main/java/com/ccinc/ui/product_card/navigation/Navigation.kt
+++ ui/src/main/java/com/ccinc/ui/product_card/navigation/Navigation.kt
@@ -27,36 +27,3 @@ internal fun NavGraphBuilder.productCardScreen(onBack: () -> Unit) {
         ProductCardRoute(onBack = onBack, products = products)
     }
 }
-
-//fun NavController.navigateToBranchDetails(
-//    branchesResponse: BranchesResponse?,
-//    navOptions: NavOptions? = null
-//) {
-//
-//    val branchesResponseStr = Gson().toJson(branchesResponse).replace("/", "^")
-//
-//    this.navigate("$BRANCH_DETAILS_ROUTE/$branchesResponseStr", navOptions)
-//}
-//
-//fun NavGraphBuilder.branchDetailsScreen(onBack: () -> Unit) {
-//    animatedComposableSlideHorizontal(route = "$BRANCH_DETAILS_ROUTE/{branches}") {
-//
-//        val branchesResponse = Gson().fromJson(
-//            it.arguments?.getString("branches").checkForNull().replace("^", "/"),
-//            BranchesResponse::class.java
-//        )
-//
-//        BranchesDetailsRoute(branchesResponse = branchesResponse, onBack = onBack)
-//    }
-
-
-//    composable(route = "${BRANCH_DETAILS_ROUTE}/{branches}") {
-//
-//        val branchesResponse = Gson().fromJson(
-//            it.arguments?.getString("branches").checkForNull().replace("^", "/"),
-//            BranchesResponse::class.java
-//        )
-//
-//        BranchesDetailsRoute(branchesResponse = branchesResponse, onBack = onBack)
-//    }
-//}
